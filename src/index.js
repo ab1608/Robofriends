416b3c24b99684f485401ae4f2071841a7edb1db
ab1608 <abebriones98@gmail.com>
2022-06-15 17:50:59 -0400

    Learned about components and component properties.

--- src/index.js
+++ src/index.js
@@ -1,14 +1,33 @@
+/**
+ * React allows us to make components that only need to concern themselves with their
+ * own functionality (javascript) and styles (css)
+ */
+
+
+
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import Hello from './Hello.js';
+// import App from './App'
 import reportWebVitals from './reportWebVitals';
+import 'tachyons';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
+
+/*
+We can add properties to our components that can be used in the class.js file.
+ */
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <React.StrictMode>
+        <Hello
+            greeting={'Hello' + 'I\'m learning react'}
+            name={'abraham'}
+        />
+
+        {/*<App/>*/}
+    </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
