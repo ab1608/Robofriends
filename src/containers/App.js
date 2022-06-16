d876fc25ec28c91a5fbae8673b8a894febaa89a0
ab1608 <abebriones98@gmail.com>
2022-06-16 12:11:10 -0400

    added components and containers

---
+++ src/containers/App.js
@@ -0,0 +1,65 @@
+import React from 'react';
+import CardList from "../components/CardList";
+import SearchBox from '../components/SearchBox'
+import ScrollBox from '../components/ScrollBox'
+
+import '../App.css'
+
+
+/*
+The App is the only thing that can change the state.
+When all the state changes have been processed, it then passes
+down the information to the child components.
+It is now a "Smart" components. Smart components are made as classes
+and, they are known as Containers.
+
+Components that do not modify the state of other components are pure components.
+ */
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            allRobots: [],
+            searchField: '',
+        }
+    }
+
+    componentDidMount() {
+        fetch('https://jsonplaceholder.typicode.com/users')
+            .then(response => {
+                return response.json();
+            })
+            .then(users => {
+                this.setState({allRobots: users})
+            });
+    }
+
+    onSearchChange = (event) => {
+        this.setState({searchField: event.target.value});
+    }
+
+    render() {
+        const { allRobots, searchField } = this.state;
+        const filteredRobots = allRobots.filter(robot =>
+            robot.name.toLowerCase().includes(searchField.toLowerCase()));
+
+        if (!allRobots.length) {
+            return <h1>Loading</h1>
+        } else {
+            return (
+                <div className={'tc'}>
+                    <h1 className={'f1'}>RoboFriends</h1>
+                    <SearchBox searchChange={this.onSearchChange}></SearchBox>
+                    <ScrollBox>
+                        <CardList robots={filteredRobots}></CardList>
+                    </ScrollBox>
+
+                </div>
+            );
+        }
+
+    }
+}
+
+export default App;
+
